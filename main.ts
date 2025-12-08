c7c5db1be2618727afdf2b6c50f27533a2213710
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2025-12-07 22:45:35 -0600

    Agregando modo cluster (corregido)

--- main.ts
+++ main.ts
@@ -1,30 +1,69 @@
+import os from 'os';
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import cluster from 'cluster';
+import { setupMaster, setupWorker } from '@socket.io/sticky';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 import { socketHandler } from './src/infrastructure/socket.io/handlers/SocketHandler';
 import { startDependencies } from './src/infrastructure/dependencies';
 import { startRabbitConsumer } from './src/infrastructure/rabbitMQ/adapters/RabbitMQ';
 
-const app = express();
-const httpServer = createServer(app);
-const io = new Server (httpServer, {
-    "cors": { "origin": "*" },
-});
+const nCpus = os.cpus().length;
 
-startDependencies(io)
+if (cluster.isPrimary) {
+    console.log(`Worker maestro ${process.pid} en ejecución`);
+    const app = express();
+    const httpServer = createServer(app);
+    setupMaster(httpServer, {
+        loadBalancingMethod: "least-connection"
+    });
+    setupPrimary();
 
-io.on("connection", (socket) => {
-    console.log("Usuario conectado:", socket.id);
-
-    socket.on('join_room', (room: string) => {
-        socket.join(room); // Siendo la room, el ID del usuario
+    cluster.setupPrimary({
+        serialization: "advanced"
     });
 
-    socketHandler(socket);
-});
+    httpServer.listen(8000, () => {
+        console.log("Socket.io en línea");
+    });
 
-startRabbitConsumer();
+    for (let i = 0; i < nCpus; i++)
+        cluster.fork();
 
-httpServer.listen(8000, () => {
-    console.log("Socket.io en línea");
-});
+    cluster.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} terminado`);
+        cluster.fork();
+    });
+} else {
+    console.log(`Worker ${process.pid} inicializado`);
+
+    const app = express();
+    const httpServer = createServer(app);
+
+    const io = new Server (httpServer, {
+        "cors": { "origin": "*" },
+    });
+
+    io.adapter(createAdapter());
+
+    setupWorker(io);
+
+    startDependencies(io)
+
+    io.on("connection", (socket) => {
+        console.log("Usuario conectado:", socket.id);
+
+        socket.on('join_room', (room: string) => {
+            socket.join(room); // Siendo la room, el ID del usuario
+        });
+
+        socketHandler(socket);
+    });
+
+    startRabbitConsumer();
+
+    httpServer.listen(8000, () => {
+        console.log("Socket.io en línea");
+    });
+}
